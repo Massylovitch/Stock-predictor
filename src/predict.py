8d4104ce044c1bf22106a1d61164c802c5031007
Massyl GH <massyl-6@hotmail.fr>
2025-04-06 18:33:30 +0200

    update prediction script

--- src/predict.py
+++ src/predict.py
@@ -88,39 +88,37 @@ async def predict(request):
 
 
 if __name__ == "__main__":
-    print(os.getcwd())
-    if os.getcwd() == "stock-predictor":
-        item = {
-            "price_24_hour_ago": 46656.851562,
-            "price_23_hour_ago": 46700.535156,
-            "price_22_hour_ago": 46700.535156,
-            "price_21_hour_ago": 46700.535156,
-            "price_20_hour_ago": 46700.535156,
-            "price_19_hour_ago": 46700.535156,
-            "price_18_hour_ago": 46700.535156,
-            "price_17_hour_ago": 46700.535156,
-            "price_16_hour_ago": 46700.535156,
-            "price_15_hour_ago": 46700.535156,
-            "price_14_hour_ago": 46700.535156,
-            "price_13_hour_ago": 46700.535156,
-            "price_12_hour_ago": 46700.535156,
-            "price_11_hour_ago": 46700.535156,
-            "price_10_hour_ago": 46700.535156,
-            "price_9_hour_ago": 46700.535156,
-            "price_8_hour_ago": 46700.535156,
-            "price_7_hour_ago": 46700.535156,
-            "price_6_hour_ago": 46700.535156,
-            "price_5_hour_ago": 46700.535156,
-            "price_4_hour_ago": 46700.535156,
-            "price_3_hour_ago": 46700.535156,
-            "price_2_hour_ago": 46700.535156,
-            "price_1_hour_ago": 46700.535156,
-        }
+    item = {
+        "price_24_hour_ago": 46656.851562,
+        "price_23_hour_ago": 46700.535156,
+        "price_22_hour_ago": 46700.535156,
+        "price_21_hour_ago": 46700.535156,
+        "price_20_hour_ago": 46700.535156,
+        "price_19_hour_ago": 46700.535156,
+        "price_18_hour_ago": 46700.535156,
+        "price_17_hour_ago": 46700.535156,
+        "price_16_hour_ago": 46700.535156,
+        "price_15_hour_ago": 46700.535156,
+        "price_14_hour_ago": 46700.535156,
+        "price_13_hour_ago": 46700.535156,
+        "price_12_hour_ago": 46700.535156,
+        "price_11_hour_ago": 46700.535156,
+        "price_10_hour_ago": 46700.535156,
+        "price_9_hour_ago": 46700.535156,
+        "price_8_hour_ago": 46700.535156,
+        "price_7_hour_ago": 46700.535156,
+        "price_6_hour_ago": 46700.535156,
+        "price_5_hour_ago": 46700.535156,
+        "price_4_hour_ago": 46700.535156,
+        "price_3_hour_ago": 46700.535156,
+        "price_2_hour_ago": 46700.535156,
+        "price_1_hour_ago": 46700.535156,
+    }
 
-        # uvicorn.run("src.predict:app", port=5000, log_level="info")
+    # uvicorn.run("src.predict:app", port=5000, log_level="info")
 
-        from fastapi.testclient import TestClient
+    from fastapi.testclient import TestClient
 
-        client = TestClient(app)
-        response = client.post("/predict/", json={"instances": item})
-        assert response.status_code == 200
+    client = TestClient(app)
+    response = client.post("/predict/", json={"instances": item})
+    assert response.status_code == 200
