23e5fc5d294465b05df4932f6f7db0cff0f75b5a
Massyl GH <massyl-6@hotmail.fr>
2025-04-06 18:27:02 +0200

    update prediction script

--- src/predict.py
+++ src/predict.py
@@ -1,62 +1,69 @@
 import pandas as pd
-from cerebrium import get_secret
-import pickle
 from comet_ml import API
 from sklearn.pipeline import Pipeline
-
+import os
 from src.utils.logger import get_console_logger
+from dotenv import load_dotenv
+from fastapi import FastAPI
+import uvicorn
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+import pickle
 
 
-logger = get_console_logger('deployer')
+load_dotenv()
+logger = get_console_logger("deployer")
 
-try:
-    # this code works when running on Cerebrium
-    COMET_ML_WORKSPACE = get_secret("COMET_ML_WORKSPACE")
-    COMET_ML_API_KEY = get_secret("COMET_ML_API_KEY")
-    COMET_ML_MODEL_NAME = get_secret("COMET_ML_MODEL_NAME")
+# this code works when running on Cerebrium
+app = FastAPI()
 
-except ImportError:
-    # this code works when running locally
-    import os
-    COMET_ML_WORKSPACE = os.environ['COMET_ML_WORKSPACE']
-    COMET_ML_API_KEY = os.environ['COMET_ML_API_KEY']
-    COMET_ML_MODEL_NAME = os.environ['COMET_ML_MODEL_NAME']
+
+# Health check endpoint
+@app.get("/health")
+async def health_check():
+    return {"status": "healthy"}
+
+
+COMET_ML_WORKSPACE = os.environ["COMET_ML_WORKSPACE"]
+COMET_ML_API_KEY = os.environ["COMET_ML_API_KEY"]
+COMET_ML_MODEL_NAME = os.environ["COMET_ML_MODEL_NAME"]
 
 
 def load_production_model_from_registry(
     workspace: str,
     api_key: str,
     model_name: str,
-    status: str = 'Production',
+    status: str = "Production",
 ) -> Pipeline:
     """Loads the production model from the remote model registry"""
 
     # find model version to deploy
     api = API(api_key)
-    model_details = api.get_registry_model_details(workspace, model_name)['versions']
-    model_versions = [md['version'] for md in model_details if md['status'] == status]
+    model_details = api.get_registry_model_details(workspace, model_name)["versions"]
+    model_versions = [md["version"] for md in model_details if md["status"] == status]
 
     if len(model_versions) == 0:
-        logger.error('No production model found')
-        raise ValueError('No production model found')
+        logger.error("No production model found")
+        raise ValueError("No production model found")
     else:
-        logger.info(f'Found {status} model versions: {model_versions}')
+        logger.info(f"Found {status} model versions: {model_versions}")
         model_version = model_versions[0]
-    
+
     # download model from comet ml registry to local file
     api.download_registry_model(
         workspace,
         registry_name=model_name,
         version=model_version,
-        output_path='./',
-        expand=True
+        output_path="./",
+        expand=True,
     )
-    
+
     # load model from local file to memory
-    with open('./model.pkl', "rb") as f:
+    with open("./model.pkl", "rb") as f:
         model = pickle.load(f)
-    
+
     return model
+
 
 model = load_production_model_from_registry(
     workspace=COMET_ML_WORKSPACE,
@@ -64,43 +71,56 @@ model = load_production_model_from_registry(
     model_name=COMET_ML_MODEL_NAME,
 )
 
-def predict(item, run_id, logger):
+
+@app.route("/predict", include_in_schema=False, methods=["GET", "POST"])
+async def predict(request):
+    body = await request.json()
+    instances = body["instances"]
 
     # transform item to dataframe
-    df = pd.DataFrame([item])
+    df = pd.DataFrame([instances])
 
     # predict
     prediction = model.predict(df)[0]
 
-    return {"prediction": prediction}
+    json_compatible_item_data = jsonable_encoder(prediction)
+    return JSONResponse(content=json_compatible_item_data)
 
-if __name__ == '__main__':
-    item = {
-        'price_24_hour_ago': 46656.851562,
-        'price_23_hour_ago': 46700.535156,
-        'price_22_hour_ago': 46700.535156,
-        'price_21_hour_ago': 46700.535156,
-        'price_20_hour_ago': 46700.535156,
-        'price_19_hour_ago': 46700.535156,
-        'price_18_hour_ago': 46700.535156,
-        'price_17_hour_ago': 46700.535156,
-        'price_16_hour_ago': 46700.535156,
-        'price_15_hour_ago': 46700.535156,
-        'price_14_hour_ago': 46700.535156,
-        'price_13_hour_ago': 46700.535156,
-        'price_12_hour_ago': 46700.535156,
-        'price_11_hour_ago': 46700.535156,
-        'price_10_hour_ago': 46700.535156,
-        'price_9_hour_ago': 46700.535156,
-        'price_8_hour_ago': 46700.535156,
-        'price_7_hour_ago': 46700.535156,
-        'price_6_hour_ago': 46700.535156,
-        'price_5_hour_ago': 46700.535156,
-        'price_4_hour_ago': 46700.535156,
-        'price_3_hour_ago': 46700.535156,
-        'price_2_hour_ago': 46700.535156,
-        'price_1_hour_ago': 46700.535156
-    }
 
-    prediction = predict(item, None, None)
-    print(f'{prediction=}')
+if __name__ == "__main__":
+    print(os.getcwd())
+    if os.getcwd() == "stock-predictor":
+        item = {
+            "price_24_hour_ago": 46656.851562,
+            "price_23_hour_ago": 46700.535156,
+            "price_22_hour_ago": 46700.535156,
+            "price_21_hour_ago": 46700.535156,
+            "price_20_hour_ago": 46700.535156,
+            "price_19_hour_ago": 46700.535156,
+            "price_18_hour_ago": 46700.535156,
+            "price_17_hour_ago": 46700.535156,
+            "price_16_hour_ago": 46700.535156,
+            "price_15_hour_ago": 46700.535156,
+            "price_14_hour_ago": 46700.535156,
+            "price_13_hour_ago": 46700.535156,
+            "price_12_hour_ago": 46700.535156,
+            "price_11_hour_ago": 46700.535156,
+            "price_10_hour_ago": 46700.535156,
+            "price_9_hour_ago": 46700.535156,
+            "price_8_hour_ago": 46700.535156,
+            "price_7_hour_ago": 46700.535156,
+            "price_6_hour_ago": 46700.535156,
+            "price_5_hour_ago": 46700.535156,
+            "price_4_hour_ago": 46700.535156,
+            "price_3_hour_ago": 46700.535156,
+            "price_2_hour_ago": 46700.535156,
+            "price_1_hour_ago": 46700.535156,
+        }
+
+        # uvicorn.run("src.predict:app", port=5000, log_level="info")
+
+        from fastapi.testclient import TestClient
+
+        client = TestClient(app)
+        response = client.post("/predict/", json={"instances": item})
+        assert response.status_code == 200
